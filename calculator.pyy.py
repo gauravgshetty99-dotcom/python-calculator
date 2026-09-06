eea8ad0f136c97d51a0c8ed736865aacc3eed887
gauravgshetty99-dotcom <gauravgshetty99@gmail.com>
2026-09-06 15:24:27 +0530

    Add files via upload
    calculator program

---
+++ calculator.pyy.py
@@ -0,0 +1,13 @@
+n=10
+m=5
+operation=input("enter operation(+,-,*,/):")
+if operation=="+":
+    print(n+m)
+elif operation=="-":
+    print(n-m)
+elif operation=="*":
+    print(n*m)
+elif operation=="/":
+    print(n/m)
+else:
+    print("invalid operation:")
